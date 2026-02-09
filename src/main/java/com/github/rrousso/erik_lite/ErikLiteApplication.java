e911941446c248a3b79bb45c868e5e95ba730d80
Rafael Rousso <167447046+rrousso@users.noreply.github.com>
2026-02-09 01:07:07 -0300

    Init commit

---
+++ src/main/java/com/github/rrousso/erik_lite/ErikLiteApplication.java
@@ -0,0 +1,13 @@
+package com.github.rrousso.erik_lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErikLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErikLiteApplication.class, args);
+	}
+
+}
